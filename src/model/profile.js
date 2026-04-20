e0b76ad9c161e22d1431a18bb99f8f2fd7deee35
DevTech <micahkesiena@gmail.com>
2026-04-20 15:37:07 +0100

    feat: added new fields to allow the implementation of filtering, sorting and pagination

--- src/model/profile.js
+++ src/model/profile.js
@@ -1,17 +1,18 @@
 import mongoose from "mongoose"
 
 const ProfileSchema = new mongoose.Schema({
-  _id: { type: String }, // Storing UUID v7 as string
+  _id: { type: String, required: true }, // UUID v7
   name: { type: String, required: true, lowercase: true, unique: true },
-  gender: { type: String, required: true },
+  gender: { type: String, required: true, enum: ["male", "female", "unknown"] },
   gender_probability: { type: Number },
-  sample_size: { type: Number },
   age: { type: Number, required: true },
-  age_group: { type: String, required: true },
-  country_id: { type: String, required: true },
+  age_group: { type: String, required: true, enum: ["child", "teenager", "adult", "senior"] },
+  country_id: { type: String, required: true, uppercase: true }, // ISO code (2 letters)
+  country_name: { type: String, required: true },
   country_probability: { type: Number },
-  is_confident: { type: Boolean, default: true },
-  created_at: { type: Date, default: () => new Date() }
+  sample_size: { type: Number }, // Keeping for completeness
+  is_confident: { type: Boolean, default: true }, // Keeping for completeness
+  created_at: { type: Date, default: Date.now }
 }, { 
   toJSON: {
     transform: (doc, ret) => {
